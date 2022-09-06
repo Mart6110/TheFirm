298878899aacc7a588b6d632de3f565615589624
mart6110 <martinus.mortensen@gmail.com>
2022-09-06 12:13:27 +0200

    Done

--- TheFirm/Program.cs
+++ TheFirm/Program.cs
@@ -10,9 +10,9 @@ namespace TheFirm
     {
         static void Main(string[] args)
         {
-            Sweeper sweeper = new Sweeper("Jens", "Jensen", 2006903030);
-            Manager manager = new Manager("Lars", "Kaspersen", 2710872020, 90807060);
-            ProjectManager projectmanager = new ProjectManager("Kasper", "Larsen", 1201851010, 20304050, "KJ@firm.com");
+            Employee sweeper = new Sweeper("Jens", "Jensen", 2006903030);
+            Employee manager = new Manager("Lars", "Kaspersen", 2710872020, 90807060);
+            Employee projectmanager = new ProjectManager("Kasper", "Larsen", 1201851010, 20304050, "KJ@firm.com");
 
             sweeper.GetProperties();
             manager.GetProperties();
